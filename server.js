78461bb5d46d316d02e4198f4e35966884ad18e4
Ramya B. Nayak <ramyanayak@gmail.com>
2022-02-03 13:09:54 +0530

    publish

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var cors = require('cors');
-const routes = require('./shared/routes');
+const routes = require('./backend/shared/routes');
 var app = express();
 var port = process.env.PORT || 42069;
 app.use(express.json())
@@ -12,6 +12,15 @@ routes.register.common(app);
 routes.register.user(app);
 routes.register.company(app);
 routes.register.category(app);
+
+
+app.use(express.static('dist/pd'));
+
+app.get('/*',function(req,res){
+		let reqPath = path.join(__dirname, 'dist/pd/index.html');
+		res.sendFile(reqPath);
+	});
+
 
 
 app.listen(port, ()=>{
